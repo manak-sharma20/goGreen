f88d5ed7765fff0e4f3caea3ed9e08c77a018389
Kumar Manak <manakkash20@gmail.com>
2022-01-10 15:08:10 +0000

    Commit on 2022-01-10T15:08:10.000Z

--- commit.js
+++ commit.js
@@ -5,39 +5,50 @@ import random from "random";
 
 const git = simpleGit();
 
-// Load existing commit data
-let commitData = JSON.parse(fs.readFileSync("./commit-data.json", "utf8"));
+// === CONFIG ===
+const authorName = "Kumar Manak"; // Your GitHub name
+const authorEmail = "manakkash20@gmail.com"; // Your GitHub-linked email
+const branch = "main"; // Default branch of your fork
 
-// Function to create commits for a specific date
+// Load or create commit-data.json
+const jsonFile = "./commit-data.json";
+let commitData = { commits: [] };
+if (fs.existsSync(jsonFile)) {
+  commitData = JSON.parse(fs.readFileSync(jsonFile, "utf8"));
+}
+
+// Function to create backdated commits for a date
 async function createCommitsForDate(date, count) {
   for (let i = 0; i < count; i++) {
+    // Random time for the day
     const timestamp = moment(date)
       .hour(random.int(0, 23))
       .minute(random.int(0, 59))
       .second(random.int(0, 59))
       .toISOString();
 
-    // Add timestamp to JSON
+    // Update JSON
     commitData.commits.push({ timestamp });
+    fs.writeFileSync(jsonFile, JSON.stringify(commitData, null, 2));
 
-    // Write JSON after each commit
-    fs.writeFileSync(
-      "./commit-data.json",
-      JSON.stringify(commitData, null, 2)
-    );
-
-    // Write to a temp file to trigger git change
+    // Temporary file to trigger change
     fs.writeFileSync("temp-file.txt", `Commit at: ${timestamp}`);
 
-    // Create backdated commit
+    // Commit with backdated timestamp and correct author
     await git.add(".");
     await git.commit(`Commit on ${timestamp}`, {
       "--date": timestamp,
+      "--author": `"${authorName} <${authorEmail}>"`,
     });
 
     console.log(`Created commit: ${timestamp}`);
   }
+
+  // Push all commits automatically
+  await git.push("origin", branch);
+  console.log("All commits pushed to GitHub!");
 }
 
-// Example: Create 5 commits on Jan 10, 2022
-createCommitsForDate("2025-01-10", 5);
+// === EXAMPLE USAGE ===
+// Create 5 commits on Jan 10, 2022
+createCommitsForDate("2022-01-10", 5);
